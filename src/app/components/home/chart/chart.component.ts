e81e3219d27a52f28d3b73148a2e0700d4c7f7da
Saevar2000 <saevar@bitar.is>
2020-03-30 17:19:21 +0000

    Disable trading temporarily

--- src/app/components/home/chart/chart.component.ts
+++ src/app/components/home/chart/chart.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Renderer2, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { createChart } from 'lightweight-charts';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild, AfterViewInit, HostListener, NgZone } from '@angular/core';
+import { createChart, CrosshairMode } from 'lightweight-charts';
 import { BitarApiService } from '../../../services/bitar-api.service';
 import { ChartPair } from '../../../services/chartPair';
 import { formatDate, formatNumber } from '@angular/common';
@@ -11,73 +11,82 @@ import { formatDate, formatNumber } from '@angular/common';
 })
 export class ChartComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('one') d1: ElementRef;
+
   OhlcChart: ChartPair;
   pri: number;
   date: Date = new Date(2001, 20, 3);
   tada: number = 2000;
 
-  @ViewChild('one') d1: ElementRef;
+  container = document.createElement('div');
 
-  constructor(private renderer: Renderer2, private bitar: BitarApiService) { }
+  chart = createChart(this.container, {
+    width: 1000,
+    height: 470,
+    layout: {
+      fontFamily: 'Open Sans'
+    },
+    localization: {
+      locale: 'is',
+      timeFormatter: function (time) {
+        var t = new Date(time.year, time.month, time.day);
+        return formatDate(t, 'mediumDate', 'is');
+      },
+      priceFormatter: function (price) {
+        return formatNumber(Math.trunc(price), 'is');
+      }
+    },
+    priceScale: {
+      borderColor: 'rgba(197, 203, 206, 1)',
+      borderVisible: false,
+      autoScale: true,
+      entireTextOnly: true,
+    },
+    timeScale: {
+      borderVisible: false
+    },
+    grid: {
+      horzLines: {
+        visible: false,
+        color: '#eee',
+      },
+      vertLines: {
+        visible: false,
+        color: '#ffffff',
+      },
+    },
+    crosshair: {
+      horzLine: {
+        visible: true,
+        labelVisible: true
+      },
+      vertLine: {
+        visible: true,
+        // style: 0,
+        // width: 2,
+        // color: 'rgba(32, 38, 46, 0.1)',
+        labelVisible: true,
+      }
+    },
+  });
+
+  constructor(private renderer: Renderer2, private ngZone: NgZone, private bitar: BitarApiService) {
+    window.onresize = (e) => {
+      // ngZone.run will help to run change detection.
+      this.ngZone.run(() => {
+        console.log("Width: " + window.innerWidth);
+        console.log("Height: " + window.innerHeight);
+      });
+    };
+  }
 
   ngAfterViewInit() {
     this.bitar.GetChartPair().subscribe(res => {
       this.OhlcChart = res;
 
-      const container = document.createElement('div');
-      this.renderer.appendChild(this.d1.nativeElement, container);
+      this.renderer.appendChild(this.d1.nativeElement, this.container);
 
-      const chart = createChart(container, {
-        width: 1200,
-        height: 600,
-        layout: {
-          fontFamily: 'Open Sans'
-        },
-        localization: {
-          locale: 'is',
-          timeFormatter: function (time) {
-            var t = new Date(time.year, time.month, time.day);
-            return formatDate(t, 'mediumDate', 'is');
-          },
-          priceFormatter: function (price) {
-            return formatNumber(Math.trunc(price), 'is');
-          }
-        },
-        priceScale: {
-          borderColor: 'rgba(197, 203, 206, 1)',
-          borderVisible: false,
-          autoScale: true,
-          entireTextOnly: true,
-        },
-        timeScale: {
-          borderVisible: false
-        },
-        grid: {
-          horzLines: {
-            visible: false,
-            color: '#eee',
-          },
-          vertLines: {
-            visible: false,
-            color: '#ffffff',
-          },
-        },
-        crosshair: {
-          horzLine: {
-            visible: true,
-            labelVisible: true
-          },
-          vertLine: {
-            visible: true,
-            // style: 0,
-            // width: 2,
-            // color: 'rgba(32, 38, 46, 0.1)',
-            labelVisible: true,
-          }
-        },
-      });
-
-      const series = chart.addAreaSeries({
+      const series = this.chart.addAreaSeries({
         topColor: 'rgba(33, 150, 243, 0.56)',
         bottomColor: 'rgba(33, 150, 243, 0.04)',
         lineColor: 'rgba(33, 150, 243, 1)',
@@ -97,4 +106,16 @@ export class ChartComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    var width = <number>event.target.innerWidth;
+    var height = <number>event.target.innerHeight;
+    var width2 = this.d1.nativeElement.offsetWidth;
+    var height2 = this.d1.nativeElement.offsetHeight;
+    console.log('width ' + width2);
+    console.log('height ' + height2);
+    this.chart.resize(width2, height2);
+  }
+
 }
