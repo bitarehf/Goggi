d28b88d4c9fae27ec75458a9b3f052eba1b1453b
codefactor-io <support@codefactor.io>
2020-04-13 05:07:28 +0000

    [CodeFactor] Apply fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,11 @@ import localeIs from '@angular/common/locales/is';
 registerLocaleData(localeIs, 'is');
 
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { TooltipModule } from 'ng2-tooltip-directive';
+import { AboutComponent } from './components/about/about.component';
+import { ChangelogComponent } from './components/changelog/changelog.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DepositComponent } from './components/dashboard/deposit/deposit.component';
 import { OverviewComponent } from './components/dashboard/overview/overview.component';
 import { SettingsComponent } from './components/dashboard/settings/settings.component';
 import { TransactionsComponent } from './components/dashboard/transactions/transactions.component';
@@ -31,18 +35,14 @@ import { IskCompletedComponent } from './components/dashboard/withdrawal-complet
 import { BtcComponent } from './components/dashboard/withdrawal/btc/btc.component';
 import { IskComponent } from './components/dashboard/withdrawal/isk/isk.component';
 import { WithdrawalComponent } from './components/dashboard/withdrawal/withdrawal.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/home/register/register.component';
-import { OrderCompletedComponent } from './components/order-completed/order-completed.component';
-import { DepositComponent } from './components/dashboard/deposit/deposit.component';
-import { AboutComponent } from './components/about/about.component';
-import { ChartComponent } from './components/home/chart/chart.component';
-import { HeaderTransparentComponent } from './components/header-transparent/header-transparent.component';
 import { ExchangeRateComponent } from './components/exchange-rate/exchange-rate.component';
 import { FaqComponent } from './components/faq/faq.component';
+import { HeaderTransparentComponent } from './components/header-transparent/header-transparent.component';
 import { HelpComponent } from './components/help/help.component';
-import { ChangelogComponent } from './components/changelog/changelog.component';
-import { TooltipModule } from 'ng2-tooltip-directive';
+import { ChartComponent } from './components/home/chart/chart.component';
+import { RegisterComponent } from './components/home/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { OrderCompletedComponent } from './components/order-completed/order-completed.component';
 
 
 
