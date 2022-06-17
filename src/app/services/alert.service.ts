d8de2c3ffdc4a532c3d1b87e56fc9172678148d7
Saevar2000 <saevar@bitar.is>
2022-06-17 22:55:03 +0000

    Grammar

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -9,12 +9,12 @@ export class AlertService {
     private subject = new Subject<Alert>();
     private defaultId = 'default-alert';
 
-    // enable subscribing to alerts observable
+    // Enable subscribing to alerts observable.
     onAlert(id = this.defaultId): Observable<Alert> {
         return this.subject.asObservable().pipe(filter(x => x && x.id === id));
     }
 
-    // convenience methods
+    // Convenience methods.
     success(message: string, options?: any) {
         this.alert(new Alert({ ...options, type: AlertType.Success, message }));
     }
@@ -31,13 +31,13 @@ export class AlertService {
         this.alert(new Alert({ ...options, type: AlertType.Warning, message }));
     }
 
-    // main alert method    
+    // Main alert method    
     alert(alert: Alert) {
         alert.id = alert.id || this.defaultId;
         this.subject.next(alert);
     }
 
-    // clear alerts
+    // Clear alerts
     clear(id = this.defaultId) {
         this.subject.next(new Alert({ id }));
     }
